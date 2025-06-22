99bf8dbe05b3c3c2c9a18e095b45d7f93dc3ce4c
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-06-22 13:25:47 +0900

    feat: update site-header

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -12,25 +12,20 @@ export function SiteHeader() {
   return (
     <header className="bg-background sticky top-0 z-50 w-full">
       <div className="container-wrapper px-6">
-        <div className="container flex h-12 items-center justify-between border-b md:h-20">
-          <Link
-            href="/"
-            className="hover:bg-accent/20 flex items-center gap-4 rounded-md p-2"
-            aria-label="Return to top page"
-            title="Return to top page"
-          >
-            <Image
-              src="/images/avatars/cakegaly.webp"
-              alt="cakegaly icon"
-              width={32}
-              height={32}
-              className="ring-border rounded-full ring-1"
-              priority={true}
-            />
-            <span className="hidden font-mono text-sm tracking-widest md:block">
-              {siteConfig.name}
-            </span>
-          </Link>
+        <div className="container flex h-12 items-center gap-2 border-b **:data-[slot=separator]:!h-4 md:h-16">
+          <Button asChild variant="ghost" size="icon" className="flex size-10">
+            <Link href="/">
+              <Image
+                src="/images/avatars/cakegaly.webp"
+                alt="cakegaly icon"
+                width={32}
+                height={32}
+                className="ring-border rounded-full ring-1"
+                priority={true}
+              />
+              <span className="sr-only">{siteConfig.name}</span>
+            </Link>
+          </Button>
           <div className="ml-auto flex items-center gap-2 md:flex-1 md:justify-end">
             <Button
               asChild
