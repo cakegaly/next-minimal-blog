a609b7f042fa212f08e263018390f0193a167789
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-06-22 14:59:40 +0900

    feat: update link card component

--- src/components/shared/link-card.tsx
+++ src/components/shared/link-card.tsx
@@ -25,7 +25,7 @@ export function LinkCard({
 }: LinkCardProps) {
   return (
     <Card className="group overflow-hidden rounded-none border-none py-2 shadow-none">
-      <Link href={link} prefetch={false} aria-label={`記事を読む: ${title}`}>
+      <Link href={link} aria-label={`Read more: ${title}`}>
         {variant === 'vertical' && (
           <>
             <CardHeader className="p-0">
@@ -40,7 +40,7 @@ export function LinkCard({
                 />
               </AspectRatio>
             </CardHeader>
-            <CardContent className="space-y-2 p-0 pt-3 group-hover:opacity-50">
+            <CardContent className="space-y-2 p-3 pb-0 group-hover:opacity-50">
               <p className="line-clamp-2 text-base">{title}</p>
               {description && (
                 <p className="line-clamp-2 text-xs">{description} </p>
@@ -103,7 +103,7 @@ export function LinkCard({
           </CardContent>
         )}
         {variant === 'mini' && (
-          <CardContent className="p-0">
+          <CardContent className="px-1 py-0">
             <div className="flex gap-2">
               <div className="relative w-1/5">
                 <AspectRatio ratio={1 / 1} className="">
@@ -128,44 +128,6 @@ export function LinkCard({
                 )}
               </div>
             </div>
-          </CardContent>
-        )}
-        {variant === 'internal' && (
-          <CardContent className="rounded-sm border p-2">
-            <div className="flex items-center gap-2">
-              <div className="relative w-1/4 md:w-1/6">
-                <AspectRatio ratio={1 / 1} className="">
-                  <Image
-                    src={imageUrl}
-                    alt={`${title}のサムネイル画像`}
-                    fill
-                    className="object-cover transition-opacity group-hover:opacity-50"
-                    sizes="(min-width: 1024px) 18vw, (min-width: 768px) 25vw, 30vw"
-                    priority={priority}
-                  />
-                </AspectRatio>
-              </div>
-              <div className="w-3/4 space-y-1 md:w-5/6">
-                <p className="line-clamp-2 text-sm font-bold group-hover:opacity-50 md:text-base">
-                  {title}
-                </p>
-              </div>
-            </div>
-          </CardContent>
-        )}
-        {variant === 'hero' && (
-          <CardContent className="group relative flex aspect-[300/128] items-center justify-center overflow-hidden p-0">
-            <Image
-              src={imageUrl}
-              alt={`${title}のサムネイル画像`}
-              fill
-              className="object-cover transition-transform duration-300 group-hover:scale-110"
-              sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
-            />
-            <div className="bg-primary/40 absolute inset-0 opacity-100 transition-opacity duration-300" />
-            <p className="text-card absolute inset-0 flex items-center justify-center p-4 text-center text-base opacity-100 transition-opacity duration-300">
-              {title}
-            </p>
           </CardContent>
         )}
       </Link>
